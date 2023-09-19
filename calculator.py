59cc9f4f10f5db5ae244fffd5d825ca068d8d9b1
terencebruwer <terence@teqnet.co.za>
2023-09-19 16:40:58 +0100

    play files

---
+++ calculator.py
@@ -0,0 +1,4 @@
+x = float(input("What is x? "))
+y = float(input("What is y? "))
+
+print(f"{round(x + y,4):,}")
---
+++ hello.py
@@ -0,0 +1,6 @@
+def hello():
+    print("Hello")
+
+name = input("What's your name? ")
+hello()
+print(name)
